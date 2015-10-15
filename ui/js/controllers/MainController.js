c41d7e81b00d29fca9cc4a658ebd36d9c99d91c6
dragsava <Dragos.Sava@misys.com>
2015-10-15 21:51:48 +0300

    finished login service

--- ui/js/controllers/MainController.js
+++ ui/js/controllers/MainController.js
@@ -1,11 +1,25 @@
-var MainController = function($scope, $http , $location, $document) {
+var MainController = function($scope, $http , $location, $document, authService) {
 	$scope.showRegister = true;
 	$scope.hideLogin = true;
 	$scope.isRegister = true;
 	$scope.isLogin = false;
+	$scope.registerStatus = false;
+	$scope.loginStatus = false;
 	
 	$scope.loginClass = "login fadeInDown";
 	$scope.registerClass = "register fadeInDown";
+	
+	$scope.userLoginData = {
+		email: "",
+		password: "",
+		token: ""
+	};
+	
+	$scope.userRegisterData = {
+		username: "",
+		email: "",
+		password: ""
+	};
 	
 	$scope.onSigninClick = function(){
 		$scope.isRegister = false;
@@ -14,16 +28,21 @@ var MainController = function($scope, $http , $location, $document) {
 		$scope.registerClass = "register fadeOutDown";
 	};
 	$scope.onRegisterClick = function(){
+		authService.register($scope.userRegisterData).then(function(response){
+			console.log("......."+response);
+			$scope.registerStatus = response == "SUCCESS";
+		});	
 		$scope.isRegister = false;
 		$scope.isLogin = true;
 		$scope.hideLogin = false;
 		$scope.registerClass = "register fadeOutDown";
 	};
 	$scope.onLoginClick = function(){
-		$location.path('.#/menu');
-	};
-	$scope.onTokenClick = function(){
-		
+		authService.login($scope.userLoginData).then(function(response){
+			console.log("......."+response);
+			$scope.loginStatus = response === "SUCCESS";
+			console.log(response == "\"SUCCESS\"");
+		});	
 	};
 };
 app.controller("MainController", MainController);
--- ui/js/services/loginService.js
+++ ui/js/services/loginService.js
@@ -4,8 +4,26 @@
 	TODO pretty much everything
 */
 
-var parseService = function($http) {
+var authService = function($http) {
 
+	var postLogin = function(loginData){
+		var url = "http://bucd464:9081/user/login";
+		console.log(loginData);
+		return $http.post(url, loginData).then(function(response){
+			console.log(response.data);
+			return response.data;
+        });
+	};
+	
+	var postRegister = function(registerData){
+		var url = "http://bucd464:9081/user/sigin";
+		console.log(registerData);
+		return $http.post(url, registerData).then(function(response){
+			console.log(response.data);
+			return response.data;
+        });
+	};
+	
     var getUser = function(username){
 		var url = ""; // TODO
         return $http.get(url.concat(username))
@@ -24,8 +42,10 @@ var parseService = function($http) {
 	
     return {
         getUser: getUser,
-		getPicture: getPicture
+		getPicture: getPicture,
+		login: postLogin,
+		register: postRegister
 		//TO be continued
     };
 };
-app.factory('parse', parseService);
+app.factory('authService', authService);
